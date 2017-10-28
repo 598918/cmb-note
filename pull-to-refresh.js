57ccb62c2f8dd29091c3d945eb68fd736055570a
598918 <598918@qq.com>
2017-10-28 18:34:59 +0800

    Create pull-to-refresh.js

---
+++ pull-to-refresh.js
@@ -0,0 +1,140 @@
+(function($) {
+    'use strict';
+    $.initPullToRefresh = function(pageContainer) {
+        var eventsTarget = $(pageContainer);
+        if (!eventsTarget.hasClass('pull-to-refresh-content')) {
+            eventsTarget = eventsTarget.find('.pull-to-refresh-content');
+        }
+        if (!eventsTarget || eventsTarget.length === 0) return;
+
+        var isTouched, isMoved, touchesStart = {},
+            isScrolling, touchesDiff, container, refresh = false,
+            useTranslate = false,
+            startTranslate = 0,
+            translate, scrollTop, wasScrolled, triggerDistance, dynamicTriggerDistance;
+
+        container = eventsTarget;
+
+        // Define trigger distance
+        if (container.attr('data-ptr-distance')) {
+            dynamicTriggerDistance = true;
+        } else {
+            triggerDistance = 44;
+        }
+
+        function handleTouchStart(e) {
+            isMoved = false;
+            isTouched = true;
+            isScrolling = undefined;
+            wasScrolled = undefined;
+            touchesStart.x = e.type === 'touchstart' ? e.originalEvent.targetTouches[0].pageX : e.pageX;
+            touchesStart.y = e.type === 'touchstart' ? e.originalEvent.targetTouches[0].pageY : e.pageY;
+            container = $(this);
+        }
+
+        function handleTouchMove(e) {
+            if (!isTouched) return;
+            isScrolling = true;
+            
+            scrollTop = container[0].scrollTop;
+            if (typeof wasScrolled === 'undefined' && scrollTop !== 0) wasScrolled = true;
+            
+            var pageY = e.type === 'touchmove' ? e.originalEvent.targetTouches[0].pageY : e.pageY;
+
+            if (!isMoved) {
+                container.removeClass('transitioning');
+                if (scrollTop > container[0].offsetHeight) {
+                    isTouched = false;
+                    return;
+                }
+                if (dynamicTriggerDistance) {
+                    triggerDistance = container.attr('data-ptr-distance');
+                    if (triggerDistance.indexOf('%') >= 0) triggerDistance = container[0].offsetHeight * parseInt(triggerDistance, 10) / 100;
+                }
+                startTranslate = container.hasClass('refreshing') ? triggerDistance : 0;
+                useTranslate = true;
+            }
+            isMoved = true;
+            touchesDiff = pageY - touchesStart.y;
+
+            if (touchesDiff > 0 && scrollTop <= 0 || scrollTop < 0) {
+                if (useTranslate) {
+                    e.preventDefault();
+                    translate = (Math.pow(touchesDiff, 0.85) + startTranslate);
+                    container.transform('translate3d(0,' + translate + 'px,0)');
+                } else {}
+                if ((useTranslate && Math.pow(touchesDiff, 0.85) > triggerDistance) || (!useTranslate && touchesDiff >= triggerDistance * 2)) {
+                    refresh = true;
+                    container.addClass('pull-up').removeClass('pull-down');
+                } else {
+                    refresh = false;
+                    container.removeClass('pull-up').addClass('pull-down');
+                }
+            } else {
+                container.removeClass('pull-up pull-down');
+                refresh = false;
+                return;
+            }
+        }
+
+        function handleTouchEnd() {
+            if (!isTouched || !isMoved) {
+                isTouched = false;
+                isMoved = false;
+                return;
+            }
+            if (translate) {
+                container.addClass('transitioning');
+                translate = 0;
+            }
+            container.transform('');
+            if (refresh) {
+                //防止二次触发
+                if(container.hasClass('refreshing')) return;
+                container.addClass('refreshing');
+                container.trigger('refresh');
+            } else {
+                container.removeClass('pull-down');
+            }
+            isTouched = false;
+            isMoved = false;
+        }
+
+        // Attach Events
+        eventsTarget.on($.touchEvents.start, handleTouchStart);
+        eventsTarget.on($.touchEvents.move, handleTouchMove);
+        eventsTarget.on($.touchEvents.end, handleTouchEnd);
+
+
+        function destroyPullToRefresh() {
+            eventsTarget.off($.touchEvents.start, handleTouchStart);
+            eventsTarget.off($.touchEvents.move, handleTouchMove);
+            eventsTarget.off($.touchEvents.end, handleTouchEnd);
+        }
+        eventsTarget[0].destroyPullToRefresh = destroyPullToRefresh;
+
+    };
+    $.pullToRefreshDone = function(container) {
+        $(window).scrollTop(0);//解决微信下拉刷新顶部消失的问题
+        container = $(container);
+        if (container.length === 0) container = $('.pull-to-refresh-content.refreshing');
+        container.removeClass('refreshing').addClass('transitioning');
+        container.transitionEnd(function() {
+            container.removeClass('transitioning pull-up pull-down');
+        });
+    };
+    $.pullToRefreshTrigger = function(container) {
+        container = $(container);
+        if (container.length === 0) container = $('.pull-to-refresh-content');
+        if (container.hasClass('refreshing')) return;
+        container.addClass('transitioning refreshing');
+        container.trigger('refresh');
+    };
+
+    $.destroyPullToRefresh = function(pageContainer) {
+        pageContainer = $(pageContainer);
+        var pullToRefreshContent = pageContainer.hasClass('pull-to-refresh-content') ? pageContainer : pageContainer.find('.pull-to-refresh-content');
+        if (pullToRefreshContent.length === 0) return;
+        if (pullToRefreshContent[0].destroyPullToRefresh) pullToRefreshContent[0].destroyPullToRefresh();
+    };
+})(jQuery);
